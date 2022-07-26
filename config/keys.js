fe5c22ad3ba8b50cb54c45f65ab4fc5d00c77c13
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2022-07-26 08:31:49 -0400

    fix: remove config file with keys

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '525143678064-tq26kvnkdqnqudgfk1pnjqocnidoucgq.apps.googleusercontent.com',
-    googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://sunnysuri28:3210762ps@cluster0.eth8kof.mongodb.net/emailprojectprod?retryWrites=true&w=majority',
-    cookieKey: 'F4DAC6CAB4691693C357391CB1789',
-}
